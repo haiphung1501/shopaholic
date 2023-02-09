ce3b936b6c15bc4134b4f6e3f6cc822aad6c5c83
haiphung1501 <haiphung15012002@gmail.com>
2023-02-09 20:10:19 +0700

    update product controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,7 +2,11 @@ const Product = require("../models/product");
 
 const productController = {
     getAllProduct : async (req,res) => {
-        console.log("Success")
+        try {
+            const products = await Product.find();
+            res.status(200).send(products);
+        } catch (err) {res.status(500).json(err)}
+
     },
 
     createProduct : async (req, res) => {
